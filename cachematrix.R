c3e85227e59e664301fce55ca54a5b0b309d2e69
belanpe <belanpe@yahoo.com>
2014-09-20 14:55:54 -0400

    Updated functions contents
    Validated contents of the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,11 @@
 # Purpose:
 #   R-language script to create a matrix to be inverted
 # Synopsis:
-#  makeCacheMatrix(
+# 	amat <- makeCacheMatrix()
+# 	amat$set(matrix(c(1,2,3,4),nrow=2,ncol=,byrow=TRUE))
+# 	amat$get()
+# 	cacheSolve(amat)
 #
-#  cacheSolve(x,...)
-#
-amat <- matrix(c(1,2,3,4),nrow=2,ncol=,byrow=TRUE)
-amat
-t(amat)
-solve(amat)
 
 
 #  Date   ID		Description
@@ -20,35 +17,32 @@ solve(amat)
 #
 
 
-# Write a short comment describing this function
+#
+#  Define matrix functions:
 # 
-makeCacheMatrix <- function(x = matrix()) {
-	# 
+
+makeCacheMatrix <- function(x,...) {
+	#
+	#  Ensure matrix is square:
 	m <- NULL
-    set <- function(y) {
+    set <- function(matrix(y)) {
         x <<- y
         m <<- NULL
     }
-    get <- function() x
-    #setmean <- function(mean) m <<- mean
-    #getmean <- function() m
-    #list(set = set, get = get,
-    #     setmean = setmean,
-    #     getmean = getmean)
-	setinvert <- function(mean) m <<- mean
+	get <- function() x
+	setinvert <- function(solve) m <<- solve
     getinvert <- function() m
+	#  Broadcast results:
     list(set = set, get = get,
          setinvert = setinvert,
          getinvert = getinvert)
 }
 
-
 #
-# Write a short comment describing this function
+# Resolve the cached square matrix
 #
 cacheSolve <- function(x, ...) {
 	# Return a matrix that is the inverse of 'x'
-	#m <- x$getmean()
 	m <- x$getinvert()
     if(!is.null(m)) {
         message("getting cached data")
